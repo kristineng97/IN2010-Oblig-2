9fb0d278013111a47f84159bdf985defad7f4bf5
Kristin Eng <kristineng@eduroam-193-157-240-33.wlan.uio.no>
2021-10-05 17:04:10 +0200

    Implement problem 1 (correct output for number of nodes and edges)

--- oblig2.py
+++ oblig2.py
@@ -1,19 +1,10 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from collections import defaultdict
-import graphviz
-import os
-import string
-
 
 def read_data(filename="movies.tsv"):
     l = []
-    with open("movies.tsv", "r") as infile:
+    with open(filename, "r") as infile:
         for line in infile:
             l.append(line.strip().split("\t"))
-    return l[:10]
-
-
+    return l
 
 
 class Movie:
@@ -26,30 +17,43 @@ class Movie:
     def add_actor(self, actor):
         self.actors.append(actor)
 
+    def __str__(self):
+        return f"{self.title} ({self.tt_id}): {' | '.join([str(actor) for actor in self.actors])}"
+
+
 class Actor:
     def __init__(self, nm_id, name, *movies):
         self.nm_id = nm_id
         self.name = name
         self.movies = movies
 
-
+    def __str__(self):
+        return f"{self.name}:{self.movies}"
 
 
 def main():
     movie_lines = read_data("movies.tsv")
-    movies = [Movie(*line[:-1]) for line in movie_lines]
+    all_movies = {line[0]: Movie(*line[:-1]) for line in movie_lines}
 
     actor_lines = read_data("actors.tsv")
-    actors = [Actor(*line) for line in actor_lines]
+    all_actors = {line[0]: Actor(*line) for line in actor_lines}
 
-    
+    for actor in all_actors.values():
+        for tt_id in actor.movies:
+            if tt_id in all_movies:
+                all_movies[tt_id].add_actor(actor)
 
+    count_edges = 0
+    for movie in all_movies.values():
+        count_edges += len(movie.actors)*(len(movie.actors) - 1)/2
 
+    for i,movie in enumerate(all_movies.values()):
+        if i > 10:
+            break
+        print(movie)
 
-
-
-
-
+    print(len(all_actors))
+    print(count_edges)
 
 if __name__ == '__main__':
     main()
