6f327b8dcb27eb85cd479e4bf835f8f9e6512f8d
Anine Lome <aninelo@uio.no>
2021-10-05 18:14:39 +0200

    Fixed nice output to Problem 1

--- oblig2.py
+++ oblig2.py
@@ -32,28 +32,40 @@ class Actor:
 
 
 def main():
-    movie_lines = read_data("movies.tsv")
-    all_movies = {line[0]: Movie(*line[:-1]) for line in movie_lines}
+    # Problem 1
+    movie_lines = read_data("movies.tsv") # Read lines in movies.tsv to list movie_lines containing [tt_id, title, rating, num. of voices]
+    all_movies = {line[0]: Movie(*line[:-1]) for line in movie_lines} # Make a dictionary of all movies with keys tt_id and values is Movie-objects
 
-    actor_lines = read_data("actors.tsv")
-    all_actors = {line[0]: Actor(*line) for line in actor_lines}
+    actor_lines = read_data("actors.tsv") # Read lines in actors.tsv to list actor_lines containing [nm_id, name, tt_id1, tt_id2, ... etc]
+    all_actors = {line[0]: Actor(*line) for line in actor_lines} # Make a dictionary of all actors with keys nm_id and values is Actor-objects
 
+    # Loop through all Actor-objects, and for each movie the actor contributes in,
+    # first check that this movie is contained in movies.tsv,
+    # then go to this movie object and add given actor to actors-list
     for actor in all_actors.values():
         for tt_id in actor.movies:
             if tt_id in all_movies:
                 all_movies[tt_id].add_actor(actor)
+    # Testing:
+    #for i,movie in enumerate(all_movies.values()):
+    #    if i > 3:
+    #        break
+    #    print(movie)
 
+    # This gives us a nice way to access all the necessary adjacency relations
+
+    # We want the actors to be the nodes in our graph, so numbers of nodes is the length of all_actors-dict
+    # Now we want to count the number of edges in the graph:
     count_edges = 0
     for movie in all_movies.values():
         count_edges += len(movie.actors)*(len(movie.actors) - 1)/2
 
-    for i,movie in enumerate(all_movies.values()):
-        if i > 10:
-            break
-        print(movie)
+    # This gives the result:
+    print(f"Oppgave 1\n\nNodes: {len(all_actors)}\nEdges: {int(count_edges)}\n")
 
-    print(len(all_actors))
-    print(count_edges)
+    # Problem 2
+
+
 
 if __name__ == '__main__':
     main()
