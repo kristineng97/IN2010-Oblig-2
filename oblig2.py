937b1a579ed3b125f349ecff7d9ede577c911551
didriksi <didriksten@gmail.com>
2021-10-06 13:54:21 +0200

    Finish implementation of problem 2: bfs
    Created a neighbours attribute for the Actor class, that on
    demand finds all its neighbors, meaning all actors that play
    in the same movie as them.
    
    Then, breadth first search is implemented, and a somewhat messy
    detangling of the actor and movie information is relayed to the
    main block, where it is printed out.
    
    Needs some cleaning up, but works fine now.

--- oblig2.py
+++ oblig2.py
@@ -1,3 +1,4 @@
+from queue import Queue
 
 def read_data(filename="movies.tsv"):
     l = []
@@ -18,26 +19,81 @@ class Movie:
         self.actors.append(actor)
 
     def __str__(self):
-        return f"{self.title} ({self.tt_id}): {' | '.join([str(actor) for actor in self.actors])}"
+        return f"{self.title}"
 
 
 class Actor:
-    def __init__(self, nm_id, name, *movies):
+    def __init__(self, all_movies, nm_id, name, *movies):
         self.nm_id = nm_id
         self.name = name
-        self.movies = movies
+        self.movies = [all_movies[tt_id] for tt_id in movies if tt_id in all_movies]
+
+    @property
+    def neighbors(self):
+        if "_neighbors" in self.__dict__:
+            return self._neighbors
+        else:
+            self._neighbors = {}
+            self.build_adjacency()
+            return self._neighbors
 
     def __str__(self):
-        return f"{self.name}:{self.movies}"
+        return f"{self.name}"
 
+    def __eq__(self, other_actor):
+        return self.nm_id == other_actor.nm_id
 
-def shortest_path(nmid1, nmid2):
+    def __hash__(self):
+        return self.nm_id.__hash__()
 
-    return
+    def build_adjacency(self):
+        for movie in self.movies:
+            other_actors = (o_actor for o_actor in movie.actors if o_actor != self)
+            for other_actor in other_actors:
+                if other_actor not in self._neighbors:
+                    self._neighbors[other_actor] = {movie}
+                else:
+                    self._neighbors[other_actor].add(movie)
 
+def breadth_first_search(from_actor, to_actor):
+    parents = {from_actor: None}
+    queue = Queue()
+    queue.put(from_actor)
+    result = []
 
+    while queue:
+        actor = queue.get()
+        result.append(actor)
+        for other_actor, common_movies in actor.neighbors.items():
+            if other_actor == to_actor:
+                parents[other_actor] = (actor, next(iter(common_movies)))
+                
+                return parents
 
+            elif other_actor not in parents:
+                parents[other_actor] = (actor, next(iter(common_movies)))
+                queue.put(other_actor)
 
+    return parents
+
+def build_path_from_search_results(search_results, from_actor, to_actor):
+    """
+
+    Return: List of two-tuples (actor, movie they shared with the actor in the next tuple)
+    """
+    path = []
+    this = (to_actor, None)
+    while this is not None:
+        path.append(this)
+        this = search_results[this[0]]
+    
+    return path
+
+def find_shortest_path(from_actor, to_actor):
+    search_results = breadth_first_search(from_actor, to_actor)
+    path = build_path_from_search_results(search_results, from_actor, to_actor)
+
+    return path
 
 def main():
     # Problem 1
@@ -45,15 +101,15 @@ def main():
     all_movies = {line[0]: Movie(*line[:-1]) for line in movie_lines} # Make a dictionary of all movies with keys tt_id and values is Movie-objects
 
     actor_lines = read_data("actors.tsv") # Read lines in actors.tsv to list actor_lines containing [nm_id, name, tt_id1, tt_id2, ... etc]
-    all_actors = {line[0]: Actor(*line) for line in actor_lines} # Make a dictionary of all actors with keys nm_id and values is Actor-objects
+    all_actors = {line[0]: Actor(all_movies, *line) for line in actor_lines} # Make a dictionary of all actors with keys nm_id and values is Actor-objects
 
     # Loop through all Actor-objects, and for each movie the actor contributes in,
     # first check that this movie is contained in movies.tsv,
     # then go to this movie object and add given actor to actors-list
     for actor in all_actors.values():
-        for tt_id in actor.movies:
-            if tt_id in all_movies:
-                all_movies[tt_id].add_actor(actor)
+        for movie in actor.movies:
+            movie.add_actor(actor)
+
     # Testing:
     #for i,movie in enumerate(all_movies.values()):
     #    if i > 3:
@@ -72,16 +128,21 @@ def main():
     # This gives the result:
     print(f"Oppgave 1\n\nNodes: {len(all_actors)}\nEdges: {int(count_edges)}\n")
 
-    # Problem 2
-    for movie in all_movies.values():
-        for actor in movie.actors:
-            movie.actors.remove(actor)
-            adj_l = [actress.nm_id for actress in movie.actors]
-            E = {actor.nm_id: adj_l}
-            movie.actors.append(actor)
+    actor_pair_ids = [("nm2255973", "nm0000460")
+                    , ("nm0424060", "nm0000243")
+                    , ("nm4689420", "nm0000365")
+                    , ("nm0000288", "nm0001401")
+                    , ("nm0031483", "nm0931324")]
+    for from_actor_id, to_actor_id in actor_pair_ids:
+        from_actor = all_actors[from_actor_id]
+        to_actor = all_actors[to_actor_id]
 
-    print(E["nm0353790"])
-
+        path = find_shortest_path(from_actor, to_actor)
+        for actor, movie in reversed(path):
+            if movie is not None:
+                print(f"{actor.name}\n==[ {movie.title} ({movie.ranking}) ] ==> ", end="")
+            else:
+                print(actor.name, "\n")
 
 
 if __name__ == '__main__':
